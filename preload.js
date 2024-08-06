f16dbc1cffbd0f9ba9ac6a5d7cc79b7bcd90e857
simuhunluo <simuhunluo@outlook.com>
2024-08-07 00:01:02 +0800

    2.0.0 1. 增强识别功能，极大减少误识别的概率。比如经常复制的一大串包含类会议号的json等文本内容，新版本已经优化； 2. 会议号识别失败后，将会自动清空utools搜索框内容，可以直接重新粘贴，切回发消息提醒告知未识别到会议号； 3. 会议号识别成功后，将会自动销毁utools搜索框

--- preload.js
+++ preload.js
@@ -1,20 +1,39 @@
-
 window.exports = {
+    "open_wemeet_room": {
+        mode: 'none',
+        args: {
+            enter: (param) => {
+                const regex = /#?(?:腾讯会议：)?(\d{3})[- ]?(\d{3,4})[- ]?(\d{3,4})/;
+                let matchResult = param.payload.match(regex);
 
-  "open_wemeet_room":{
-    mode: 'none',
-    args: {
-      enter: (param) => {
-        const regex = /#?(?:腾讯会议：)?(\d{3})[- ]?(\d{3,4})[- ]?(\d{3,4})/;
-        const matchResult = param.payload.match(regex);
+                console.log(param.payload)
+                console.log(param.payload.length)
+                console.log(param.payload.indexOf("腾讯会议"))
 
-        if (matchResult) {
-          const meetingCode = matchResult.slice(1).join('');
-          const schemaUrl = `wemeet://page/inmeeting?meeting_code=${meetingCode}`;
-          window.utools.shellOpenExternal(schemaUrl);
+                if (param.payload.length > 100 && !param.payload.includes("腾讯会议")) {
+                    matchResult = false
+                    window.utools.showNotification('未识别到会议号');
+                    // 插件被选中打开后，恢复搜索框至默认状态
+                    window.utools.outPlugin(); // 退出插件，恢复默认的搜索框状态
+                }
+                // 超长字符串，只识别带短横线的数值
+                if (param.payload.length > 100){
+                    const regexLongStr = /#?(?:腾讯会议：)?(\d{3})-(\d{3,4})-(\d{3,4})/;
+                    matchResult = param.payload.match(regexLongStr);
+                }
+
+                if (matchResult) {
+                    const meetingCode = matchResult.slice(1).join('');
+                    // window.utools.showNotification('识别到会议号: ' + meetingCode);
+                    const schemaUrl = `wemeet://page/inmeeting?meeting_code=${meetingCode}`;
+                    window.utools.shellOpenExternal(schemaUrl);
+
+                    // 插件被选中打开后，恢复搜索框至默认状态
+                    window.utools.hideMainWindow(); // 隐藏主窗口
+                    window.utools.outPlugin(); // 退出插件，恢复默认的搜索框状态
+                }
+            }
         }
-      }
     }
-  }
 };
 
